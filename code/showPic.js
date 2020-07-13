34f768b8521c6a63440caa1fd0d4bfd13516c34a
sukekes <xjinhz@126.com>
2020-07-13 19:05:34 +0800

    add css

--- code/showPic.js
+++ code/showPic.js
@@ -2,11 +2,14 @@ function showPic(pic){
     var source = pic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
-    
+    // 针对元素使用，此处得是pic.getAttribute,不能用document
+    var text = pic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue  = text;
 }
 
 function countBodyChildren(){
-    var body_element = document.getElementsByTagName('body')[0];
+    var body_element = pic.getElementsByTagName('body')[0];
     // alert(body_element.childNodes.length)
     alert(body_element.nodeType)
 }
